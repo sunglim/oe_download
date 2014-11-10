2063a143b69a6494b8b38081550829a4e07a2f7d
root <root@sungguk-R570-U-AFLGL.(none)>
2014-11-10 16:38:55 +0900

    Fix release

--- tools/ex.sh
+++ tools/ex.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
 #mount -t smbfs //156.147.69.51/work /mnt/51server -o username=sungguk.lim
 echo "* COPY"
+rm hybridtv*.bz2;
+rm hybridtv-atsc -rf;
 cp /mnt/24server/*.bz2 ./
 tar xvf hybridtv*.bz2
 cp ./hybridtv-atsc/usr/lib/* ./usr/lib
