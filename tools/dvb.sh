a5e71ec65fca4189b1f3d6c7d172443a04cfb55c
root <root@sungguk-R570-U-AFLGL.(none)>
2014-11-19 10:37:58 +0900

    Deploy script update

--- tools/dvb.sh
+++ tools/dvb.sh
@@ -5,7 +5,7 @@ echo "* COPY"
 rm hybridtv*.bz2;
 rm hybridtv-dvb -rf;
 
-FILENAME=$(ls -lrt /mnt/24server/hybridtv-dvb*.bz2 | awk -F" " '{ print $9 }')
+FILENAME=$(ls -lrt /mnt/24server/lg1311/sungguk.lim/hybridtv-dvb*.bz2 | head -1| awk -F" " '{ print $9 }')
 cp $FILENAME ./
 tar xvf hybridtv*.bz2
 cp ./hybridtv-dvb/usr/lib/* ./usr/lib
