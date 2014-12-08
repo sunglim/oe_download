7b74bc84914bd4e8e39faaec34273f111404a5b1
root <root@sungguk-R570-U-AFLGL.(none)>
2014-12-08 09:12:36 +0900

    35server

--- tools/atsc.sh
+++ tools/atsc.sh
@@ -5,7 +5,7 @@ echo "* COPY"
 rm hybridtv*.bz2;
 rm hybridtv-atsc -rf;
 
-FILENAME=$(ls -lrt /mnt/24server/lg1311/sungguk.lim/hybridtv-atsc*.bz2 | head -1| awk -F" " '{ print $9 }')
+FILENAME=$(ls -lrt /mnt/35server/lg1311/sungguk.lim/hybridtv-atsc*.bz2 | head -1| awk -F" " '{ print $9 }')
 cp $FILENAME ./
 tar xvf hybridtv*.bz2
 cp ./hybridtv-atsc/usr/lib/* ./usr/lib
